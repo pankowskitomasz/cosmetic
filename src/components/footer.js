6c73456ed2d822617869f7bc243396c33eff2969
pankowskitomasz <pankowski_tomasz@o2.pl>
2021-07-30 21:44:11 +0200

    frontend: contact,register,login

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,12 +8,12 @@ class Footer extends Component{
     render(){
         return(      
             <footer>
-                <Container fluid className="d-flex align-items-center p-3 fixed-bottom z-index-0">
+                <Container fluid className="d-flex align-items-center p-3 bg-footer border-top border-success z-index-0">
                     <Row className="mx-auto text-center d-flex w-100">
                         <Col xs={10} className="mx-auto">
-                            <small className="my-0 text-dark">
+                            <small className="my-0 text-success">
                                 Copyright &copy; 2021 Tomasz Pankowski. All rights reserved. 
-                                <Link to={this.props.privacyLink.href} className="text-dark text-decoration-none">
+                                <Link to={this.props.privacyLink.href} className="text-success text-decoration-none">
                                     {this.props.privacyLink.name}
                                 </Link>
                             </small>
---
+++ src/views/products.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+
+class Products extends Component{
+    render(){
+        return(          
+            <main className="minh-100vh">     
+
+            </main>
+        );
+    }
+}
+
+export default Products;
